9dd450004aaf094c87ed73dbeac8c38795c4e679
TheNesur <5400maxime@gmail.com>
2023-11-15 15:09:59 +0100

    Update main.ts

--- main/main.ts
+++ main/main.ts
@@ -7,7 +7,7 @@ function initializationMaps(x: number, y: number): Array<Array<number>> {
             map[i][j] = 0;
         }
     }
-
+    // console.log(map);
     return map;
 }
 
@@ -22,8 +22,8 @@ function displayMap(map: Array<Array<number>>): void {
         for (let y = 0; y < map[i].length; y ++) {
             switch(map[i][y]) {
                 case 0 : ligne+= "- | "; break;
-                case 1 : ligne += "X | "; break; // joueur 1 = X
-                case 2 : ligne += "0 | "; break; //  
+                case 1 : ligne += "0 | "; break; // joueur 1 = X
+                case 2 : ligne += "X | "; break; //  
             }
         }
         console.log(ligne);
@@ -39,7 +39,7 @@ function isVoid(posHorizontale:number, posVertical:number, map:Array<Array<numbe
 // PARTIE ARTHUR //
 ///////////////////
 
-function addPion(posVertical:number, player:boolean, map:Array<Array<number>>): boolean {
+function addPion(posVertical:number, player:number, map:Array<Array<number>>): number {
     for (let posHorizontale = map.length-1; posHorizontale >= 0; posHorizontale--) {
         /*console.log("addPion : ",isVoid(posHorizontale, posVertical, map),
         "posHorizontale=",posHorizontale, 
@@ -48,11 +48,11 @@ function addPion(posVertical:number, player:boolean, map:Array<Array<number>>): 
         */
         
         if (isVoid(posHorizontale, posVertical, map)) {
-            map[posHorizontale][posVertical] = Number(player)+1;
-            return true;
+            map[posHorizontale][posVertical] = player;
+            return posHorizontale;
         }
     }
-    return false;
+    return -1;
 }
 
 ///////////////////
@@ -66,7 +66,7 @@ function isAlign(posHorizontale: number, posVertical: number, map: Array<Array<n
     let player = map[posHorizontale][posVertical];
     if (player === 0) return false;
 
-    // Vérification horizontale
+    // Vérification Verticale [-]
     for (let y = posVertical - 3; y <= posVertical + 3; y++) {
         if (y >= 0 && y < map[0].length && map[posHorizontale][y] === player) {
             nbrAlign++;
@@ -77,75 +77,172 @@ function isAlign(posHorizontale: number, posVertical: number, map: Array<Array<n
             nbrAlign = 0;
         }
     }
+    nbrAlign = 0;
+    // Vérification horizontale [|]
+    for (let horizontale = posHorizontale-3; horizontale <= posHorizontale+3; horizontale++) {
+        if (horizontale >= 0 && horizontale < map.length && map[horizontale][posVertical] === player) {
+            nbrAlign++
+            if (nbrAlign === 4) {
+                return true;
+            }
+        } else {
+            nbrAlign = 0;
+        }
+    }
+
+
+
+    nbrAlign = 0;
+    // Vérification diagonale [\]
+    for (let horizontale = posHorizontale-3, vertical = posVertical-3; 
+        vertical <= posVertical+3; 
+        horizontale++, vertical++
+        ) {
+        // console.log("[x;y] = [",horizontale,";",vertical,"]");
+
+        if ((horizontale >= 0 && horizontale < map.length) 
+        && (vertical >= 0 && vertical < map[0].length) 
+        && map[horizontale][vertical] === player) 
+        {
+            // console.log("Player : ",player, " is ", map[horizontale][vertical]);
+
+            nbrAlign++;
+            if (nbrAlign === 4) {
+                return true;
+            } 
+        } else {
+            nbrAlign = 0;
+        }
+    }
+
+    
+    // Vérification diagonale [/]
+    for (let horizontale = posHorizontale-3, vertical = posVertical+3; 
+        vertical >= posVertical-3; 
+        horizontale++, vertical--
+        ) {
+        // console.log("[x;y] = [",horizontale,";",vertical,"]");
+
+        if ((horizontale >= 0 && horizontale < map.length) 
+        && (vertical >= 0 && vertical < map[0].length) 
+        && map[horizontale][vertical] === player) 
+        {
+            // console.log("Player : ",player, " is ", map[horizontale][vertical]);
+
+            nbrAlign++;
+            if (nbrAlign === 4) {
+                return true;
+            } 
+        } else {
+            nbrAlign = 0;
+        }
+    }
 
     return false;
 }
 
+function isWin(player:number, posHorizontale: number, posVertical: number, map: Array<Array<number>>): boolean {
+    return map[posHorizontale][posVertical] === player ?  isAlign(posHorizontale, posVertical, map) : false;
+}
 
-/*
-function isAlign(posHorizontale:number, posVertical:number, map:Array<Array<number>>) : boolean {
-    let nbrAlign = 0;
-    let player = map[posHorizontale][posVertical];
-    if (player == 0) return false;
+function menu() {
+    console.log("/-----------------------------------\\");
+    console.log(": 1- Joueur contre Joueur.          :");
+    console.log(": 2- Joueur contre IA Aléatoire.    :");
+    console.log(": 3- Joueur contre IA Récompenses.  :");
+    console.log(": 4- Joueur contre IA Grille.       :");
+    console.log("\\-----------------------------------/");
+}
 
-    for (let y = posVertical-3; y <= posVertical+3; y++) {
-        // console.log(i, posVertical, posHorizontale, map[posHorizontale][posVertical], player);
+function question(): number {
+    let choose = -1;
+    do {
+        menu();
+        choose = Number(prompt("\n\nVotre choix : "));
+        console.log(choose);
+    } while (choose == -1)
+    return choose;
+}
 
-        if ((y < map[0].length && y >= 0) && map[posHorizontale][y] == player) {
-            // console.log("isAlign : ", posHorizontale, posVertical, map[posHorizontale][posVertical], nbr , player);
-            nbrAlign++;
-            if (nbrAlign == 3) {
-                return true;
-            }
-        } else {
-            nbrAlign = 0;
+
+function playerVSplayer(map: Array<Array<number>>): void {
+
+    let player = 1;
+    do {
+        console.log(displayMap(map));
+        console.log("Au joueur",player,"de jouer.");
+        let posVerticalPion = Number(prompt("Ligne a jouer : "));
+        let posHorizontalePion = addPion(posVerticalPion, player, map);
+        if (posVerticalPion === -1) {
+            console.log("Vous n'avez pas donner une position valide, chiffre autorisé de 0 à", map.length-1);
+            continue;
         }
-    }
-    nbrAlign = 0;
-    for (let x = posHorizontale-3, y = posVertical-3; x <= posVertical+3 && y <=posHorizontale+3; x++, y++) {
-        if ((x < map[0].length && x >= 0) && (y < map.length && y >= 0)  && map[x][y] == player) {
-            nbrAlign++;
-            if (nbrAlign == 3) {
-                return true;
-            }
-        } else {
-            nbrAlign = 0;
+        else if (isWin(player, posHorizontalePion, posVerticalPion, map)) {
+            
+            console.log(displayMap(map));
+            console.log("\nJOUEUR", player, " A GAGNE !!\n");
+            break;
         }
-    }
-    nbrAlign = 0;
-    for (let x = posHorizontale+3, y = posVertical+3; x >= posVertical+3 && y >= posHorizontale+3; x--, y--) {
-        if ((x < map[0].length && x >= 0) && (y < map.length && y >= 0)  && map[x][y] == player) {
-            nbrAlign++;
-            if (nbrAlign == 3) {
-                return true;
-            }
-        } else {
-            nbrAlign = 0;
+        if (player === 1) player = 2;
+        else player = 1;
+
+    } while(true)
+}
+
+function playerVsIARandom(map: Array<Array<number>>): void {
+
+    let player = 1;
+    let posVerticalPion = 0;
+    let posHorizontalePion = 0;
+    do {
+        console.log(displayMap(map));
+        console.log("Au joueur",player,"de jouer.");
+
+        if (player == 1)
+            posVerticalPion = Number(prompt("Ligne a jouer : "));
+        else posVerticalPion = Math.floor(Math.random() * ((map[0].length-1) - 0 + 1)) + 0;
+
+
+        posHorizontalePion = addPion(posVerticalPion, player, map);
+        if (posVerticalPion === -1) {
+            console.log("Vous n'avez pas donner une position valide, chiffre autorisé de 0 à", map.length-1);
+            continue;
         }
+        else if (isWin(player, posHorizontalePion, posVerticalPion, map)) {
+            
+            console.log(displayMap(map));
+            console.log("\nJOUEUR", player, " A GAGNE !!\n");
+            break;
+        }
+        if (player === 1) player = 2;
+        else player = 1;
+
+    } while(true)
+}
+
+
+function main() {
+    console.log("INITIALISATION DU PUISSANCE 4...");
+    let map = initializationMaps(Number(prompt("Hauteur du puissance 4 :\n")),Number(prompt("largeur du puissance 4 :\n")));
+
+    switch (Number(question())) {
+        case 1:
+            console.log("Vous jouez premier jeu.");
+            playerVSplayer(map);
+            break;
+        case 2:
+            console.log("Vous jouez deuxième jeu.");
+            playerVsIARandom(map);
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            break;
+
     }
-    
-    return false;
-}*/
 
+}
 
-let maps = new Array<Array<number>>;
-maps = initializationMaps(4,5);
-console.log(displayMap(maps));
-console.log(addPion(0,true,maps));
-console.log(addPion(1,true,maps));
-console.log(addPion(2,true,maps));
-console.log(addPion(3,true,maps));
-
-// console.log(addPion(0,true,maps));
-// console.log(addPion(1,false,maps));
-// console.log(addPion(1,true,maps));
-// console.log(addPion(2,false,maps));
-// console.log(addPion(2,false,maps));
-// console.log(addPion(2,true,maps));
-// console.log(addPion(3,false,maps));
-// console.log(addPion(3,false,maps));
-// console.log(addPion(3,false,maps));
-// console.log(addPion(3,true,maps));
-console.log("Gagné ? ", isAlign(3,3,maps));
-console.log(displayMap(maps));
-// console.log(maps);
+main();
